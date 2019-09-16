f742bf756721b644de48dd4ec3d491b5fd155c27
BakaX <bagadiyi@gmail.com>
2019-09-16 17:12:04 +1200

    Update the navigator UI

--- src/Pages/HomePage/index.js
+++ src/Pages/HomePage/index.js
@@ -1,32 +1,45 @@
 import React, {Component,Fragment} from 'react';
 import 'antd/dist/antd.css';
-import {Button} from 'antd';
+import {Drawer,Button, Icon} from 'antd';
 import {Link} from 'react-router-dom'
 import './HomePage.css';
 import { Card,Carousel } from 'antd';
+import {NavBar} from "antd-mobile";
 
 class HomePage extends Component{
+    state={visible:false};
+    showDrawer = () =>{
+        this.setState({
+            visible:true,
+        });
+    };
+    onClose = () =>{
+        this.setState({
+            visible:false,
+        });
+    };
     render() {
         return(
             <Fragment>
-                <Card style={{position:"relative",float:"top"}}>
-                    <p style={{fontSize:30}}>Hi</p>
-                    <p style={{fontSize:20}}>Customer</p>
-                </Card>
-                <Carousel autoplay dots={true} style={{height:150,position:"relative",float:"top"}}>
-                    <Card bodyStyle= {{backgroundColor:"#94b8b8",height:150}} >
-                        <h3 >Promotion!!!</h3>
-                    </Card >
-                    <Card bodyStyle= {{backgroundColor:"#94b7b7",height:150}}>
-                        <h3>Huge Deal!!</h3>
-                    </Card>
-                    <Card bodyStyle= {{backgroundColor:"#94b6b6",height:150}}>
-                        <h3>Seller sell their underwear just because they are poor!!</h3>
-                    </Card>
-                    <Card bodyStyle= {{backgroundColor:"#94b5b5",height:150}}>
-                        <h3>Cheap Tree!!</h3>
-                    </Card>
-                </Carousel>
+                <div className={'NaviBar'}>
+                    {/*product list navigation bar*/}
+                    <NavBar mode = "dark"
+                            leftContent={
+                                <Icon type = "menu" style={{fontSize: '22px'}}
+                                     onClick={this.showDrawer}/>
+                            }
+                    >
+                        Product List
+                    </NavBar>
+                </div>
+                <Drawer
+                    title = "Menu"
+                    placement = "right"
+                    closable = {false}
+                    onClose = {this.onClose}
+                    visible = {this.state.visible}
+                    placement = "left"
+                >
                     <Button className={'button'}>
                         <Link to = "/treeList">
                             TreeList
@@ -45,6 +58,25 @@ class HomePage extends Component{
                             Promotion Detail
                         </Link>
                     </Button>
+                </Drawer>
+                <Card style={{position:"relative",float:"top"}}>
+                    <p style={{fontSize:30}}>Hi</p>
+                    <p style={{fontSize:20}}>Customer</p>
+                </Card>
+                <Carousel autoplay dots={true} style={{height:150,position:"relative",float:"top"}}>
+                    <Card bodyStyle= {{backgroundColor:"#94b8b8",height:150}} >
+                        <h3 >Promotion!!!</h3>
+                    </Card >
+                    <Card bodyStyle= {{backgroundColor:"#94b7b7",height:150}}>
+                        <h3>Huge Deal!!</h3>
+                    </Card>
+                    <Card bodyStyle= {{backgroundColor:"#94b6b6",height:150}}>
+                        <h3>Seller sell their underwear just because they are poor!!</h3>
+                    </Card>
+                    <Card bodyStyle= {{backgroundColor:"#94b5b5",height:150}}>
+                        <h3>Cheap Tree!!</h3>
+                    </Card>
+                </Carousel>
             </Fragment>
         )
     }
