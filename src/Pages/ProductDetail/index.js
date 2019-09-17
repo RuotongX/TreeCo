6342e54c2ae60be0ca83c89d0530f1d0809bb6ff
Dalton Chen <daltonchen2014@gmail.com>
2019-09-18 01:34:42 +1200

    Add more details to the tree detail page

--- src/Pages/ProductDetail/index.js
+++ src/Pages/ProductDetail/index.js
@@ -1,27 +1,68 @@
 import React, {Component, Fragment} from "react";
-import {NavBar} from "antd-mobile";
-import {Icon} from "antd";
+import {NavBar, WingBlank, Card, List, Button, Stepper} from "antd-mobile";
+import { Icon} from "antd";
 import {Link} from 'react-router-dom'
+import './ProductDetail.css'
 
 class ProductDetail extends Component{
 
     constructor(props){
         super(props)
+        this.state = this.props.location.treeState;
+
     }
 
     render() {
-        return(
-            <Fragment>
-                <NavBar
-                    mode = "dark"
-                    className={"NaviBar"}
-                    leftContent= {<Link to = "/treeList"><Icon className = "returnButton" type="left" /></Link>}
-                >
-                    {this.props.location.treeState.tree.productName}
-                </NavBar>
-            </Fragment>
-        )
+
+        if(this.state){
+            return(
+                <Fragment>
+                    <NavBar
+                        mode = "dark"
+                        className={"NaviBar"}
+                        leftContent= {<Link to = "/treeList"><Icon className = "returnButton" type="left" /></Link>}
+                    >
+                        {this.props.location.treeState.tree.productName}
+                    </NavBar>
+
+                    <WingBlank size={"lg"}>
+                        <Card className={"ProductCard"}>
+                            <Card.Body>
+                                <img className={'ProductImage'}
+                                     src = {require('../ProductImage/' + this.state.tree.img)}
+                                     alt = {this.state.tree.id}
+                                />
+
+                                <List className={"DetailList"}>
+                                    <List.Item extra={this.state.tree.productName}>Product Name</List.Item>
+                                    <List.Item extra={this.state.tree.drain}>Drain Speed</List.Item>
+                                    <List.Item extra={this.state.tree.sun}>Sun Condition</List.Item>
+                                    <List.Item extra={this.state.tree.maintain}>Maintenance</List.Item>
+                                    <List.Item extra={this.state.tree.height}>Maximum Height</List.Item>
+                                    <List.Item extra={this.state.tree.rate}>Grow Speed</List.Item>
+                                    <List.Item extra={this.state.tree.type}>Tree Category</List.Item>
+                                </List>
+                            </Card.Body>
+                        </Card>
+
+                        <Button type = "primary"
+                                className={"PurchaseButton"}>
+                            Add To Cart
+                        </Button>
+
+
+                    </WingBlank>
+
+
+                </Fragment>
+            )
+        } else {
+            return <div> oops </div>;
+        }
     }
+
+
+
 }
 
 export default ProductDetail;
